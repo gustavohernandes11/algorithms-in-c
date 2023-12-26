bf74ac09b7a2860eaa524cf9c772c8fdc461710a
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-12-26 11:53:42 -0300

    feat: add selection sort

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
 #include "insertion_sort.h"
+#include "selection_sort.h"
+#include "merge_sort.h"
 
+void print_array(int array[], int length) {
+    for (int i = 0; i < length; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
 
 int main() {
     int numbers[] = {1, 3, 2, 4, 7, 0};
     int length = sizeof(numbers) / sizeof(numbers[0]);
 
-    int* result = insetion_sort(numbers, length);
-
-    printf("sorted: ");
-    for (int i = 0; i < length; i++) {
-        printf("%d ", result[i]);
-    }
-    printf("\n");
-
-    free(result);
+    int* result = selection_sort(numbers, length);
+    print_array(result, length);
+    
 }
---
+++ selection_sort.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils/swap.h"
+
+int* selection_sort(int numbers[], int length){
+    int min_index, i, j;
+    for (int i = 0; i < length-1; i++)
+    {
+        min_index = i;
+        for (int j = min_index; j < length; j++)
+        {
+            if (numbers[min_index] > numbers[j]) {
+                min_index = j;
+            }
+        }
+        if (min_index != i) {
+            swap(&numbers[i], &numbers[min_index]);
+        }
+    }
+    return numbers;
+};
