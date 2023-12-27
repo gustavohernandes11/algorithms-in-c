2f3d541bff5177137266236bcc86c31c54981d0b
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-12-27 10:22:50 -0300

    feat: add merge sort

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
-#include<stdio.h>
-#include "utils/print_array.h"
+#include <stdio.h>
 #include "src/insertion_sort.h"
 #include "src/selection_sort.h"
 #include "src/merge_sort.h"
 
+int main()
+{
+    int unsorted[] = {1, 1, 1, 1, 3, 0, 0, 2, 4, 1000, 1, 7, 0, 1, 3};
+    int sorted[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int reverse[] = {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-int main() {
-    int numbers[] = {1, 3, 2, 4, 7, 0};
-    int length = sizeof(numbers) / sizeof(numbers[0]);
+    int length = sizeof(unsorted) / sizeof(unsorted[0]);
 
-    int* result = selection_sort(numbers, length);
-    print_array(result, length);
+    merge_sort(unsorted, 0, length - 1);
+    print_array(unsorted, length);
 }
---
+++ src/merge_sort.h
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../utils/print_array.h"
+
+void *merge(int numbers[], int begin, int mid, int end)
+{
+    int left_size = mid - begin + 1;
+    int right_size = end - mid;
+
+    int left[left_size], right[right_size];
+
+    for (size_t i = 0; i < left_size; i++)
+    {
+        left[i] = numbers[begin + i];
+    };
+
+    for (size_t i = 0; i < right_size; i++)
+    {
+        right[i] = numbers[mid + i + 1];
+    };
+
+    int i = 0;     // smallest right index
+    int j = 0;     // smallest left index
+    int k = begin; // actual list index to be arranged
+
+    while (j < left_size && i < right_size)
+    {
+        if (left[j] <= right[i])
+        {
+            numbers[k] = left[j];
+            j++;
+        }
+        else
+        {
+            numbers[k] = right[i];
+            i++;
+        }
+        k++;
+    }
+
+    while (j < left_size)
+    {
+        numbers[k] = left[j];
+        j++;
+        k++;
+    }
+
+    while (i < right_size)
+    {
+        numbers[k] = right[i];
+        i++;
+        k++;
+    }
+}
+
+void *merge_sort(int numbers[], int begin, int end)
+{
+    if (begin < end)
+    {
+        int mid = begin + (end - begin) / 2;
+        merge_sort(numbers, begin, mid);
+        merge_sort(numbers, mid + 1, end);
+
+        merge(numbers, begin, mid, end);
+    }
+}
